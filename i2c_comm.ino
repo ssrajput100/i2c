9d9ecb120a530aab08d891a81edc7dad99fdc32c
Sanskriti Singh Rajput <103556510+ssrajput100@users.noreply.github.com>
2023-09-26 03:26:27 +0530

    uploaded

---
+++ i2c_comm.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+#define uchar unsigned char
+uchar t;
+
+uchar data[16];
+void setup()
+{
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+  t = 0;
+}
+void loop()
+{
+  Wire.requestFrom(9, 16);    // request 16 bytes from slave device #9
+  while (Wire.available())   // slave may send less than requested
+  {
+    data[t] = Wire.read(); // receive a byte as character
+    if (t < 15)
+      t++;
+    else
+      t = 0;
+  }
+  Serial.print("data[0]:");
+  Serial.println(data[0]);
+  Serial.print("data[2]:");
+  Serial.println(data[2]);
+  Serial.print("data[4]:");
+  Serial.println(data[4]);
+  Serial.print("data[6]:");
+  Serial.println(data[6]);
+  Serial.print("data[8]:");
+  Serial.println(data[8]);
+  Serial.print("data[10]:");
+  Serial.println(data[10]);
+  Serial.print("data[12]:");
+  Serial.println(data[12]);
+  Serial.print("data[14]:");
+  Serial.println(data[14]);
+  delay(500);
+}
